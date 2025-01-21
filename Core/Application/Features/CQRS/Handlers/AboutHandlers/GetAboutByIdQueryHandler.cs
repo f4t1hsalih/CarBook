8e8451d7fb7f79a1cd5159fd7ed1e022b1e54a37
f4t1hsalih <128603000+f4t1hsalih@users.noreply.github.com>
2025-01-21 19:58:12 +0300

    GetAboutByIdQueryHandler sınıfı

---
+++ Core/Application/Features/CQRS/Handlers/AboutHandlers/GetAboutByIdQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using Application.Features.CQRS.Queries.AboutQueries;
+using Application.Features.CQRS.Results.AboutResults;
+using Application.Interfaces;
+using Domain.Entities;
+
+namespace Application.Features.CQRS.Handlers.AboutHandlers
+{
+    public class GetAboutByIdQueryHandler
+    {
+        private readonly IRepository<About> _repository;
+
+        public GetAboutByIdQueryHandler(IRepository<About> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<GetAboutByIdQueryResult> HandleAsync(GetAboutByIdQuery query)
+        {
+            var about = await _repository.GetByIdAsync(query.Id);
+            return new GetAboutByIdQueryResult
+            {
+                AboutID = about.AboutID,
+                Title = about.Title,
+                Description = about.Description,
+                ImageUrl = about.ImageUrl
+            };
+        }
+    }
+}
